4b756af2d26122aad8b3aa50574f75430fb8e51b
margri764 <62308899+margri764@users.noreply.github.com>
2021-11-16 17:37:32 -0300

    second commit

--- src/app/services/cards.service.ts
+++ src/app/services/cards.service.ts
@@ -11,20 +11,19 @@ export class CardsService {
   cardsArray= [
     {
     title:"Desarrollo Web",
-    img: "./assets/new_image.jpg",
+    img: "./assets/marketing.png",
     description: "Nuestro core está en la web, brindamos soluciones a medida o pretesteadas a la altura de los resultados buscados Webs - Ecommerce - WebApps - WordPress - y más!"
    },
    {
-    title:"Desarrollo Web",
-    img: "./assets/new_image.jpg",
-    description: "Nuestro core está en la web, brindamos soluciones a medida o pretesteadas a la altura de los resultados buscados Webs - Ecommerce - WebApps - WordPress - y más!"
+    title:"Mobile App",
+    img: "./assets/mobile.png",
+    description: "Creamos Apps para móviles y Web Apps listas para tener tu producto digital en línea. Tanto para sistema operativo iOS (Apple / iPhone) o Android (Google)."
    },
    {
-    title:"Desarrollo Web",
-    img: "./assets/new_image.jpg",
-    description: "Nuestro core está en la web, brindamos soluciones a medida o pretesteadas a la altura de los resultados buscados Webs - Ecommerce - WebApps - WordPress - y más!"
-   },
-
+    title:"Servicios Digitales",
+    img: "./assets/digital.png",
+    description: "Nuestras disciplinas incluyen un servicio completo que abarca desde el diseño y la programación de proyectos con base tecnológica, hasta las estrategias de comunicación, marketing y contenidos."
+   }
   ]  
   
   constructor() { }
